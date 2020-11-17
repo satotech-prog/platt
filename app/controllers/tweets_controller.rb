99c10aa1306bc022cae9da364778ad1eed952cf7
satotech-prog <72504810+satotech-prog@users.noreply.github.com>
2020-11-17 12:36:51 +0900

    トップページ遷移実装、表示順変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order("created_at DESC")
   end
 
   def new
