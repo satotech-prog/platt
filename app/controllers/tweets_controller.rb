0a4e3aaa5f0ef04685e1f33067f5b93e4b3b07eb
satotech-prog <72504810+satotech-prog@users.noreply.github.com>
2020-11-18 16:17:52 +0900

    ヘッダーcss作成（ヘッダーカラー）

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,6 +20,8 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
   end
 
   def destroy
