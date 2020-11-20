cb8f277b10eda6008c215e13d68b21fcfdbaa15c
satotech-prog <72504810+satotech-prog@users.noreply.github.com>
2020-11-20 16:21:30 +0900

    データベース修正

--- db/schema.rb
+++ db/schema.rb
@@ -44,15 +44,15 @@ ActiveRecord::Schema.define(version: 2020_11_18_025306) do
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "exposition", null: false
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
